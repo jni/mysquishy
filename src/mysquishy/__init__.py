00e7de5f64ccea2db3bfa979186d2bcb1de7f67b
Juan Nunez-Iglesias <jni@fastmail.com>
2023-02-21 20:01:52 +1100

    Fix sign error

--- src/mysquishy/__init__.py
+++ src/mysquishy/__init__.py
@@ -7,5 +7,5 @@ def main():
     result = squish(sys.argv[1])
     mb_before, mb_after = result / 1e6
     print(f'MB before: {mb_before}, MB after: {mb_after}, '
-          f'Saved: {mb_after - mb_before}, '
+          f'Saved: {mb_before - mb_after}, '
           f'Compression ratio: {mb_before / mb_after}')
